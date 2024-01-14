5b1bdbd17ebf25aee713168f5d7b2866f73bd8b1
Plamen Vatev <pazoff@gmail.com>
2024-01-14 13:21:34 +0200

    Add files via upload

--- google-cat.py
+++ google-cat.py
@@ -1,6 +1,5 @@
 from cat.mad_hatter.decorators import tool, hook, plugin
 from googlesearch import search
-import time
 
 
 def google_search_urls(query, num_results=3):
@@ -23,25 +22,31 @@ def before_cat_reads_message(user_message_json: dict, cat):
      num_results_to_fetch = 3
      message = message[:-1]
      print("Searching google for " + message)
-     cat.send_ws_message(content='Searching Google for ' + message, msg_type='chat')
+     cat.send_ws_message(content='Searching Google for <b>' + message + '</b> ...', msg_type='chat')
      get_search_results = google_search_urls(message, num_results_to_fetch)
      print("Results from google search: " + str(get_search_results))
-     cat.send_ws_message(content='Results for ' + message + ' from Google search:<br>' + str(get_search_results), msg_type='chat')
+     cat.send_ws_message(content='Results for <b>' + message + '</b> from Google search:<br>' + str(get_search_results), msg_type='chat')
      
      ingest_result = ""
      for i, url in enumerate(get_search_results, start=1):
         try:
             ingest_result = cat.rabbit_hole.ingest_file(cat, url, 400, 100)
             print(str(i) + ". Ingestion of " + url + " Result: Ingested")
-            cat.send_ws_message(content=str(i) + '. Ingestion of ' + url + ' Result: Ingested', msg_type='chat')
-            time.sleep(1)
+            cat.send_ws_message(content=str(i) + '. Ingestion of ' + url + ' Result: <b>Ingested</b>', msg_type='chat')
+            
         except Exception as e:
             print(str(i) + ". Ingestion of " + url + " Result: NOT Ingested")
-            cat.send_ws_message(content=str(i) + '. Ingestion of ' + url + ' Result: NOT Ingested', msg_type='chat')
+            cat.send_ws_message(content=str(i) + '. Ingestion of ' + url + ' Result: <b>NOT</b> Ingested', msg_type='chat')
 
      
-     time.sleep(1)
+     
      user_message_json["text"] = message
+     
+     print("Ingestion of URLs finished. Cheshire cat is thinking on " + message)
+     cat.send_ws_message(content='Ingestion of new data from Google search has finished.', msg_type='chat')
+     cat.send_ws_message(content='Cheshire cat is thinking on <b>' + message + '</b> ...', msg_type='chat')
 
     
     return user_message_json
+
+
